412c913e03dd643ec22e73b15419946e9aaf1aed
Forrest Baer <forrest@forrestbaer.com>
2021-10-27 10:33:09 -0500

    debounce updates, more remapping

--- config/cradio.h
+++ config/cradio.h
@@ -8,7 +8,7 @@
 #define ctRight LC(TAB)
 #define ctLeft LC(RS(TAB))
 #define rGra RG(GRAVE)
-#define rsGra RG(SH(GRAVE))
+#define rsGra RG(RS(GRAVE))
 #define reDo RG(RS(Z))
 #define unDo RG(Z)
 #define bT1 BT_SEL 1
